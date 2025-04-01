3209636b2a8012e948bde6b6a30449f366ea8d45
Yoann-Guion <yoo.guion@gmail.com>
2025-04-01 12:12:14 +0200

    feat: add activity creation

--- src/@types/activity.d.ts
+++ src/@types/activity.d.ts
@@ -1,4 +1,4 @@
-export interface Activity {
+export interface IActivity {
   id: string;
   name: string;
   weeklyGoal: number;
--- src/lib/useActivityStore.ts
+++ src/lib/useActivityStore.ts
@@ -1,18 +1,28 @@
 "use client";
 
-import { Activity } from "@/@types/activity";
-import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
 
+import { IActivity } from "@/@types/activity";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 interface ActivityStore {
-  activities: Activity[];
-  addActivity: (activity: Activity) => void;
+  activities: IActivity[];
+  addActivity: (activity: IActivity) => void;
 }
 
 export const useActivityStore = create<ActivityStore>((set) => ({
   activities: [],
 
   addActivity: (activity) =>
-    set((state) => ({
-      activities: [...state.activities, activity],
-    })),
+    set((state) => {
+      const newActivity = {
+        id: uuidv4(),
+        name: activity.name,
+        weeklyGoal: activity.weeklyGoal,
+        weeklyProgress: 0,
+        color: activity.color,
+        createdAt: new Date(),
+      };
+      return { activities: [...state.activities, newActivity] };
+    }),
 }));
