8660a69838afa4b7da5369e9bc1f404b015e8545
Yoann-Guion <yoo.guion@gmail.com>
2025-04-09 14:31:27 +0200

    feat: add persistance to the store to save the activities in the localStorage

--- src/lib/useActivityStore.ts
+++ src/lib/useActivityStore.ts
@@ -1,5 +1,6 @@
 // store/index.ts
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { ActivitySlice, createActivitySlice } from "./slices/activitySlice";
 import { createTimerSlice, TimerSlice } from "./slices/timerSlice";
 
@@ -9,7 +10,19 @@ export type StoreState = ActivitySlice & TimerSlice;
 /**
  * Creates the Zustand store for managing activities and timers
  */
-export const useActivityStore = create<StoreState>()((...a) => ({
-  ...createActivitySlice(...a),
-  ...createTimerSlice(...a),
-}));
+export const useActivityStore = create<StoreState>()(
+  persist(
+    (...a) => ({
+      ...createActivitySlice(...a),
+      ...createTimerSlice(...a),
+    }),
+    {
+      name: "activity-timer-storage",
+      storage: createJSONStorage(() => localStorage),
+      // Save only the activities to localStorage
+      partialize: (state) => ({
+        activities: state.activities,
+      }),
+    }
+  )
+);
